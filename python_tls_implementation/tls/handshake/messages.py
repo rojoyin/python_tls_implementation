508cc8929f67cae7504f261ba46068eb591ca5fa
rojoyin <rojoyin@gmail.com>
2025-04-22 09:08:00 -0500

    Add HandshakeType enum class

---
+++ python_tls_implementation/tls/handshake/messages.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+# Implementation based on RFC8446
+#https://datatracker.ietf.org/doc/html/rfc8446#page-24
+
+class HandshakeType(Enum):
+    client_hello = 1
+    server_hello = 2
+    new_session_ticket = 4
+    end_of_early_data = 5
+    encrypted_extensions = 8
+    certificate = 11
+    certificate_request = 13
+    certificate_verify = 15
+    finished = 20
+    key_update = 24
+    message_hash = 254
